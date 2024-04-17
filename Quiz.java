0c71b6474aa53fae8c07edf562b788cd8e631b99
Thamires <thamiipie@gmail.com>
2024-04-17 10:47:10 -0300

    começo do esqueleto do codigo

--- Quiz.java
+++ Quiz.java
@@ -2,7 +2,31 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class Quiz {
-    
+//métodos
+//array
 
+String[] questions= {
+    "Pergunta 1",
+    "Pergunta 2",
+    "Pergunta 3"
+};
+
+public class Quiz {
+
+    public Quiz() implements ActionListener{
+
+    }
+    public void proximaPergunta() {
+
+    }
+
+    public void ActionPerformed(ActionEvent e) {
+
+    }
+    public void displayAnswer() {
+
+    }
+    public void results() {
+
+    }
 }
