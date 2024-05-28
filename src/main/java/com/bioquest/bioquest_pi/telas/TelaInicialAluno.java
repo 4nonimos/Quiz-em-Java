f71b04b1125b0874880b305c3d9de29c5e68c4c3
mateusmats <24.00553-3@maua.br>
2024-05-27 21:38:40 -0300

    Logo BioQuest - tela aluno

--- src/main/java/com/bioquest/bioquest_pi/telas/TelaInicialAluno.java
+++ src/main/java/com/bioquest/bioquest_pi/telas/TelaInicialAluno.java
@@ -38,7 +38,7 @@ public class TelaInicialAluno extends javax.swing.JFrame {
         play2Button = new javax.swing.JButton();
         play3Button = new javax.swing.JButton();
         play4Button = new javax.swing.JButton();
-        bioquestLabel = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -73,57 +73,57 @@ public class TelaInicialAluno extends javax.swing.JFrame {
 
         play4Button.setText("jogar4");
 
-        bioquestLabel.setText("BIOQUEST FT AQUI");
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/bioquest/Group 18.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(sairButton)
-                        .addGap(86, 86, 86))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(entrarQuizButton)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(configButton)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(relatiorioButton)
-                        .addContainerGap())))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addComponent(jLabel1)
+                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(sairButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(relatiorioButton))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(play1Button)
-                        .addGap(18, 18, 18)
-                        .addComponent(play2Button)
-                        .addGap(18, 18, 18)
-                        .addComponent(play3Button)
-                        .addGap(18, 18, 18)
-                        .addComponent(play4Button))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(151, 151, 151)
-                        .addComponent(bioquestLabel)))
-                .addContainerGap(44, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(14, 14, 14)
+                                .addComponent(play1Button)
+                                .addGap(18, 18, 18)
+                                .addComponent(play2Button)
+                                .addGap(18, 18, 18)
+                                .addComponent(play3Button)
+                                .addGap(18, 18, 18)
+                                .addComponent(play4Button))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(326, 326, 326)
+                                .addComponent(entrarQuizButton))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(57, 57, 57)
+                                .addComponent(configButton)))
+                        .addGap(0, 38, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(bioquestLabel)
-                .addGap(35, 35, 35)
-                .addComponent(sairButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(relatiorioButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(16, 16, 16)
+                .addComponent(jLabel1)
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(relatiorioButton)
+                    .addComponent(sairButton))
+                .addGap(15, 15, 15)
                 .addComponent(configButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(entrarQuizButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(play1Button)
                     .addComponent(play2Button)
@@ -191,9 +191,9 @@ public class TelaInicialAluno extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel bioquestLabel;
     private javax.swing.JButton configButton;
     private javax.swing.JButton entrarQuizButton;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JButton play1Button;
     private javax.swing.JButton play2Button;
     private javax.swing.JButton play3Button;
